926b7949f98886b376e9ee2ccd76a8fd7bd8e076
Gia Kiet <hungthienlol123@gmail.com>
2026-07-26 02:35:47 +0700

    Last thật

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from app import create_app
 
 app = create_app()
@@ -9,12 +8,3 @@ if __name__ == "__main__":
         port=5000,
         debug=True
     )
-=======
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def home():
- return "<h1>Employee Task Scheduler</h1>"
-if __name__ == "__main__":
- app.run(debug=True)
->>>>>>> 4cb9ae6012a2fa03f81acee9d8b4d29d231ab13d
